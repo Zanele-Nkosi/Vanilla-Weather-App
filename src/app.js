b0549c4007b7ea48482a8f5637082e6d6a680e77
Zanele-Nkosi <134766572+Zanele-Nkosi@users.noreply.github.com>
2023-08-17 19:19:22 +0200

    Search button

--- src/app.js
+++ src/app.js
@@ -44,10 +44,25 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "d09f23af6c4b890697b8cf39be4c6279";
 let city = "Nelspruit";
+let apiKey = "d09f23af6c4b890697b8cf39be4c6279";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "d09f23af6c4b890697b8cf39be4c6279";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Nelspruit");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
