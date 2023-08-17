b1c3ff0ad03aeae2a653933670636681d288e5e5
Zanele-Nkosi <134766572+Zanele-Nkosi@users.noreply.github.com>
2023-08-17 18:47:13 +0200

    added the relevant icon from api

--- src/app.js
+++ src/app.js
@@ -29,16 +29,24 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formateDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "d09f23af6c4b890697b8cf39be4c6279";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Nelspruit&appid=${apiKey}&units=metric`;
+let city = "Nelspruit";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 
